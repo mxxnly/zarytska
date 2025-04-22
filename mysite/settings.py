c37fd4cb0fc601f393320f613ab5553e3cfb488f
mxxnly <maksskladan@gmail.com>
2025-04-22 12:28:14 +0300

    Fixed bugs.

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "pages/static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+print('хуй', BASE_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
